6ec9e9600302bc9ee92e915f93f466ead8f8e8e5
huarkiou <huarkiou@outlook.com>
2025-04-18 18:07:01 +0800

    first commit

--- GuiApp/ViewModels/OtnControlViewModel.cs
+++ GuiApp/ViewModels/OtnControlViewModel.cs
@@ -2,6 +2,6 @@
 
 public partial class OtnControlViewModel : ViewModelBase
 {
-    public string Param01 { get; } = "参数1：";
+    public double Width { get; } = 1.0;
     public string Param02 { get; } = "参数2：";
 }
---
+++ GuiApp/Views/BinarySelector.axaml.cs
@@ -0,0 +1,57 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace GuiApp.Views;
+
+public partial class BinarySelector : UserControl
+{
+    public BinarySelector()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly StyledProperty<string?> LabelTrueProperty =
+        AvaloniaProperty.Register<BinarySelector, string?>(nameof(LabelTrue));
+    public string? LabelTrue
+    {
+        get => GetValue(LabelTrueProperty);
+        set => SetValue(LabelTrueProperty, value);
+    }
+
+    public static readonly StyledProperty<string?> LabelFalseProperty =
+        AvaloniaProperty.Register<BinarySelector, string?>(nameof(LabelFalse));
+    public string? LabelFalse
+    {
+        get => GetValue(LabelFalseProperty);
+        set => SetValue(LabelFalseProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> IsTrueProperty =
+        AvaloniaProperty.Register<BinarySelector, bool>(nameof(IsTrue));
+
+    public bool IsTrue
+    {
+        get => GetValue(IsTrueProperty);
+        set => SetValue(IsTrueProperty, value);
+    }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+
+        if (change.Property == LabelTrueProperty)
+        {
+            TrueRadioButton.Content = LabelTrue ?? string.Empty;
+        }
+        else if (change.Property == LabelFalseProperty)
+        {
+            FalseRadioButton.Content = LabelFalse ?? string.Empty;
+        }
+        else if (change.Property == IsTrueProperty)
+        {
+            TrueRadioButton.IsChecked = IsTrue;
+            FalseRadioButton.IsChecked = !IsTrue;
+        }
+    }
+}
